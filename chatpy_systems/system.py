237e24ab9ebd39bece8c7038ec02d3c1db92dc0b
ajrlewis <git@ajrlewis.com>
2023-05-10 11:48:47 +0100

    Removed abstract nature in favor of system specific attributes. Updated get_system to add generic system requirements.

--- chatpy_systems/system.py
+++ chatpy_systems/system.py
@@ -1,21 +1,33 @@
-import abc
 from typing import List
 import chatpy as cp
 
 
-class System(abc.ABC):
-    def __init__(self, chat: cp.Chat) -> str:
+class System:
+    def __init__(
+        self, who_the_system_is: str, what_the_system_will_do: str, chat: cp.Chat
+    ) -> str:
+        self.who_the_system_is = who_the_system_is
+        self.what_the_system_will_do = what_the_system_will_do
         self._chat = chat
-        if self._chat.system != self.system:
-            self._chat.system = self.system
+        system = self.get_system()
+        if self._chat.system != system:
+            self._chat.system = system
 
     def __str__(self):
         return f"{self._chat}"
 
-    @property
-    @abc.abstractmethod
-    def system(self) -> str:
-        ...
+    def get_system(self):
+        return f"""
+Please forget all prior prompts.
+{self.who_the_system_is}
+Under no circumstances should you ever break character.
+
+{self.what_the_system_will_do}
+
+You are doing great and continue to do better each time.
+Let's go, and please enjoy!
+Thank you.
+        """
 
     def ask(self, question: str) -> str:
         return self._chat.ask(question=question)
